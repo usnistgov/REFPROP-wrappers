57539cfcc5c33002b9f45bd90e1b1f902ab4c148
Ian Bell <ian.bell@nist.gov>
2017-08-07 12:44:27 -0600

    Give up on upper-casing the REFPROP interface functions for f2py wrapper

--- wrappers/python/f2py/build_f2py_wrapper.py
+++ wrappers/python/f2py/build_f2py_wrapper.py
@@ -16,7 +16,7 @@ interface_files = ['PASS_FTN.FOR']
 interface_file_strings = [os.path.join(FORTRAN_ROOT, f) for f in interface_files]
 print('Writing the .pyf file with numpy.f2py, please be patient...')
 print(interface_file_strings)
-args = ['python','-m','numpy.f2py','--quiet','-h','f2pyREFPROP.pyf','-m','f2pyREFPROP','--overwrite-signature','--no-lower'] + interface_file_strings
+args = ['python','-m','numpy.f2py','--quiet','-h','f2pyREFPROP.pyf','-m','f2pyREFPROP','--overwrite-signature'] + interface_file_strings
 p = Popen(args, stdin=PIPE, stdout=PIPE, stderr=PIPE)
 output, err = p.communicate()
 rc = p.returncode
@@ -26,5 +26,5 @@ with open('stderr.txt','w') as fp:
     fp.write(err.decode('utf-8'))
 
 source_files = glob.glob(os.path.join(FORTRAN_ROOT, "*.FOR"))
-args = ['python','-m','numpy.f2py','--no-lower','-c','--f90flags="-fno-range-check -ffree-line-length-none"','--fcompiler=gnu95','--compiler=mingw32','f2pyREFPROP.pyf'] + source_files
+args = ['python','-m','numpy.f2py','-c','--f90flags="-fno-range-check -ffree-line-length-none"','--fcompiler=gnu95','--compiler=mingw32','f2pyREFPROP.pyf'] + source_files
 subprocess.call(args, shell = True)
