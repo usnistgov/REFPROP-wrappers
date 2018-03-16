0b48d4ee5d7b8a91d97cb464d32b04b7223c6a4c
Ian Bell <ian.bell@nist.gov>
2018-03-16 17:04:36 -0600

    Bump version to 0.1, push to PYPI

--- wrappers/python/ctypes/setup.py
+++ wrappers/python/ctypes/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import setuptools
 
 setup(name='ctREFPROP',
-      version='0.0',
+      version='0.1',
       description='A ctypes wrapper for NIST REFPROP library',
       author='Ian H. Bell',
       author_email='ian.bell@nist.gov',
