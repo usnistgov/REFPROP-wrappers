e96dd8c4df3682ce9a906534d32356be851e796b
DESKTOP-O0NCNID\Metin <metinyilmaz@outlook.com.tr>
2023-06-11 15:11:39 +0300

    Charp High API Wrapper

---
+++ wrappers/Csharp_High_API/RefpropDLL/Refprop.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefpropDLL
+{
+    public class Refprop
+    {
+        int iUnits = 21;
+        int iFlag = 1;
+        int iMass = 1;
+        double[] z = new double[20];
+        double[] Output = new double[200];
+        static string hUnits = new string(' ', 255);
+        double[] x = new double[20];
+        double[] y = new double[20];
+        double[] x3 = new double[20];
+        static string hpath = "C:\\Program Files (x86)\\REFPROP";
+        bool base_path = true;    
+        int iUCode = 0;
+        double q = 0;
+        static string herr = new string(' ', 255);
+        int herr_lenght = herr.Length;
+        int hUnits_length = hUnits.Length;
+
+        /// <summary>
+        /// This method sets the dll program directory.
+        /// </summary>
+        public void SetDLLPathProgramtDirectory()
+        {  
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            hpath = path;
+            base_path = false;
+           
+        }
+
+        /// <summary>
+        /// This method define fuild phase depends on given two properties like T,P,Q,etc.
+        /// </summary>
+        /// <param name="a_p">  Input Property 1, Ex.: "T" (K) </param>
+        /// <param name="a">  Input Property 1 Value, Ex.: 300 (K) </param>
+        /// <param name="b_p">  Input Property 2, Ex.: "P" (Pa) </param>
+        /// <param name="b">  Input Property 2 Value, Ex.: 300000 (Pa) </param>
+        /// <param name="hFldi">  Fluild Name, Ex.: "R134A" </param>
+        /// <returns>Desired Poperty in double type, Ex : 200000 (J/kg) </returns>
+        public string Phase(string a_p, double a, string b_p, double b, string hFldi)
+        {
+            int hpath_Lenght = hpath.Length;
+            string hFld = hFldi;
+            int ierr = 0;
+            int hFld_length = hFld.Length;
+            string hIn = a_p + b_p;
+            string hOut = "PHASE";
+            int hIn_length = hIn.Length;
+            int hOut_lenght = hOut.Length;
+            if (hOut == "Q") { hOut = "QMASS"; }
+ 
+            if (base_path)
+            {
+                Refprop_DLL_base.SETPATHdll(hpath, ref hpath_Lenght);
+                Refprop_DLL_base.SETFLUIDSdll(hFld, ref ierr, ref hFld_length);
+                Refprop_DLL_base.REFPROPdll(ref hFld, ref hIn, ref hOut, ref iUnits, ref iFlag, ref iMass, ref a, ref b, z, Output, ref hUnits, ref iUCode, x, y, x3, ref q, ref ierr, ref herr,
+                    ref hFld_length, ref hIn_length, ref hOut_lenght, ref hUnits_length, ref herr_lenght);
+            }
+
+            else
+            {
+                Refprop_DLL.SETPATHdll(hpath, ref hpath_Lenght);
+                Refprop_DLL.SETFLUIDSdll(hFld, ref ierr, ref hFld_length);
+                Refprop_DLL.REFPROPdll(ref hFld, ref hIn, ref hOut, ref iUnits, ref iFlag, ref iMass, ref a, ref b, z, Output, ref hUnits, ref iUCode, x, y, x3, ref q, ref ierr, ref herr,
+                    ref hFld_length, ref hIn_length, ref hOut_lenght, ref hUnits_length, ref herr_lenght);
+            }
+
+            string result = hUnits;
+            return result;
+
+        }
+
+        /// <summary>
+        /// This method calculates propety depends on given two properties like T,P,Q,etc.
+        /// </summary>
+        /// <param name="hOuti"> Output Property 1, Ex. : "H" (Enthalpy) </param>
+        /// <param name="a_p">  Input Property 1, Ex.: "T" (K) </param>
+        /// <param name="a">  Input Property 1 Value, Ex.: 300 (K) </param>
+        /// <param name="b_p">  Input Property 2, Ex.: "P" (Pa) </param>
+        /// <param name="b">  Input Property 2 Value, Ex.: 300000 (Pa) </param>
+        /// <param name="hFldi">  Fluild Name, Ex.: "R134A" </param>
+        /// <returns>Desired Poperty in double type, Ex : 200000 (J/kg) </returns>
+        public double Calculate(string hOuti, string a_p, double a, string b_p, double b, string hFldi)
+        {
+
+            int hpath_Lenght = hpath.Length;
+            Refprop_DLL.SETPATHdll(hpath, ref hpath_Lenght);
+            string hFld = hFldi;
+            int ierr = 0;
+            int hFld_length = hFld.Length;
+            Refprop_DLL.SETFLUIDSdll(hFld, ref ierr, ref hFld_length);
+            string hIn = a_p + b_p;
+            string hOut = hOuti;
+            int hIn_length = hIn.Length;
+            int hOut_lenght = hOut.Length;
+
+
+
+            if (hOut == "Q") { hOut = "QMASS"; } // For the quality, hOuti must be type as "QMASS"
+            if (base_path)
+            {
+                Refprop_DLL_base.REFPROPdll(ref hFld, ref hIn, ref hOut, ref iUnits, ref iFlag, ref iMass, ref a, ref b, z, Output, ref hUnits, ref iUCode, x, y, x3, ref q, ref ierr, ref herr,
+                        ref hFld_length, ref hIn_length, ref hOut_lenght, ref hUnits_length, ref herr_lenght);
+            }
+            else
+            {
+                Refprop_DLL.REFPROPdll(ref hFld, ref hIn, ref hOut, ref iUnits, ref iFlag, ref iMass, ref a, ref b, z, Output, ref hUnits, ref iUCode, x, y, x3, ref q, ref ierr, ref herr,
+                        ref hFld_length, ref hIn_length, ref hOut_lenght, ref hUnits_length, ref herr_lenght);
+            }
+
+
+            double result = Output[0];
+
+            return result;
+        }
+
+        /// <summary>
+        /// This method gives DLL version.
+        /// </summary>
+        /// <returns>Retuns type is int, Ex:10 type</returns>
+        public int DLL_VERSION()
+        {
+            int hpath_Lenght = hpath.Length;
+            Refprop_DLL.SETPATHdll(hpath, ref hpath_Lenght);
+            string hFld = "WATER";
+            int ierr = 0;
+            int hFld_length = hFld.Length;
+            Refprop_DLL.SETFLUIDSdll(hFld, ref ierr, ref hFld_length);
+            string hIn = "TP";
+            string hOut = "DLL#";
+            int hIn_length = hIn.Length;
+            int hOut_lenght = hOut.Length;
+            double a = 300;
+            double b = 101325;
+
+            if (hOut == "Q") { hOut = "QMASS"; }
+            if (base_path)
+            {
+                Refprop_DLL_base.REFPROPdll(ref hFld, ref hIn, ref hOut, ref iUnits, ref iFlag, ref iMass, ref a, ref b, z, Output, ref hUnits, ref iUCode, x, y, x3, ref q, ref ierr, ref herr,
+                    ref hFld_length, ref hIn_length, ref hOut_lenght, ref hUnits_length, ref herr_lenght);
+            }
+            else
+            {
+                Refprop_DLL.REFPROPdll(ref hFld, ref hIn, ref hOut, ref iUnits, ref iFlag, ref iMass, ref a, ref b, z, Output, ref hUnits, ref iUCode, x, y, x3, ref q, ref ierr, ref herr,
+                     ref hFld_length, ref hIn_length, ref hOut_lenght, ref hUnits_length, ref herr_lenght);
+            }
+       
+            int result = iUCode;
+            return result;
+        }
+
+    }
+}
---
+++ wrappers/Csharp_High_API/Refprop_Example/Refprop_Form.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RefpropDLL;
+using static System.Net.WebRequestMethods;
+using static System.Windows.Forms.LinkLabel;
+
+namespace Refprop_Deneme
+{
+    public partial class Refprop_Form : Form
+    {
+        public Refprop_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            Refprop refprop = new Refprop(); // creating object 
+       //     refprop.SetDLLPathProgramtDirectory();
+            string prop_1 = property_1.SelectedItem.ToString();
+            double prop_1_value = Convert.ToDouble(property_1_value.Text);
+            string prop_2 = property_2.SelectedItem.ToString();
+            double prop_2_value = Convert.ToDouble(property_2_value.Text);
+            string output_prop = output_property.Text.ToString();
+            string refri = refrigerant.SelectedItem.ToString();
+
+            switch (prop_1)
+            {
+                case "T":
+                    prop_1_value= prop_1_value+273.15;
+                    break;
+                case "P":
+                    prop_1_value = prop_1_value *(100000);
+                    break;
+                case "H":
+                    prop_1_value = prop_1_value * (1000);
+                    break;
+                case "S":
+                    prop_1_value = prop_1_value * (1000);
+                    break;
+            }
+            switch (prop_2)
+            {
+                case "T":
+                    prop_2_value = prop_2_value + 273.15;
+                    break;
+                case "P":
+                    prop_2_value = prop_2_value * (100000);
+                    break;
+                case "H":
+                    prop_2_value = prop_2_value * (1000);
+                    break;
+                case "S":
+                    prop_2_value = prop_2_value * (1000);
+                    break;
+            }
+
+            //  ("P" for Pressure, "T" for Temmperature etc..) You may find a table for input and output parameters below the ALLPROPSdll, link: https://refprop-docs.readthedocs.io/en/latest/DLL/high_level.html#function-listing
+            // prop_1 : Input Property 1
+            // prop_1_value :Input Property 1 Value
+            // prop_2 : Input Property 2
+            // prop_2_value :Input Property 2 Value
+            // refri : Refrigerant name ("R134A" , "R450A" etc.)
+            // output_prop : Output Property
+
+            double output_val = refprop.Calculate(output_prop, prop_1, prop_1_value, prop_2, prop_2_value, refri);
+         
+         
+            switch (output_prop)
+            {
+                case "T":
+                    output_val = output_val - 273.15;
+                    break;
+                case "P":
+                    output_val = output_val / (100000);
+                    break;
+                case "H":
+                    output_val = output_val / (1000);
+                    break;
+                case "S":
+                    output_val = output_val / (1000);
+                    break;
+            }
+           
+            output_property_value.Text = output_val.ToString("0.##");
+            
+            string phase = refprop.Phase(prop_1, prop_1_value, prop_2, prop_2_value, refri);
+            phase_textbox.Text = phase;
+            double dll = refprop.DLL_VERSION();
+            dll = dll / 10000;
+            dll_version_label.Text = dll.ToString("0.#"); 
+
+        }
+
+        private void property_1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string prop_1 = property_1.SelectedItem.ToString();
+
+            switch (prop_1)
+            {
+                case "T":
+                    property_1_unit.Text = "°C";
+                    break;
+                case "P":
+                    property_1_unit.Text = "Bara";
+                    break;
+                case "H":
+                    property_1_unit.Text = "kJ/kg";
+                    break;
+                case "S":
+                    property_1_unit.Text = "(kJ/kg)/K";
+                    break;
+                case "D":
+                    property_1_unit.Text = "kg/m^3";
+                    break;
+                case "Q":
+                    property_1_unit.Text = "kg-vap/kg-tot";
+                    break;
+
+            }
+        }
+
+        private void property_2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string prop_2 = property_2.SelectedItem.ToString();
+
+            switch (prop_2)
+            {
+                case "T":
+                    property_2_unit.Text = "°C";
+                    break;
+                case "P":
+                    property_2_unit.Text = "Bara";
+                    break;
+                case "H":
+                    property_2_unit.Text = "kJ/kg";
+                    break;
+                case "S":
+                    property_2_unit.Text = "(kJ/kg)/K";
+                    break;
+                case "D":
+                    property_2_unit.Text = "kg/m^3";
+                    break;
+                case "Q":
+                    property_2_unit.Text = "kg-vap/kg-tot";
+                    break;
+            }
+
+        }
+
+        private void output_property_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string output_prop = output_property.SelectedItem.ToString();
+
+            switch (output_prop)
+            {
+                case "T":
+                    output_unit.Text = "°C";
+                    break;
+                case "P":
+                    output_unit.Text = "Bara";
+                    break;
+                case "H":
+                    output_unit.Text = "kJ/kg";
+                    break;
+                case "S":
+                    output_unit.Text = "(kJ/kg)/K";
+                    break;
+                case "D":
+                    output_unit.Text = "kg/m^3";
+                    break;
+                case "Q":
+                    output_unit.Text = "kg-vap/kg-tot";
+                    break;
+            }
+        }
+    }
+}
